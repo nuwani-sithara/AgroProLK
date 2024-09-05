0ce2dd4385ec8ba58142538bd68ea5c9d54e8dbe
amith-hasintha <amithhasintha@gmail.com>
2024-09-05 18:02:46 +0530

    Add  Backend

---
+++ BACKEND/models/users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name :{
+        type : String,
+        required : true
+    },
+    address :{
+        type : String,
+        required : true
+    },
+    phoneNumber :{
+        type : Number,
+        required : true
+    },
+    email:{
+        type : String,
+        required : true
+    },
+    password :{
+        type : String,
+        required : true
+    },
+    userType :{
+        type : String,
+        required : true
+    }
+})
+
+const users = mongoose.model("users",userSchema);
+
+module.exports = users;
---
+++ BACKEND/routes/users.js
@@ -0,0 +1,87 @@
+const router = require("express").Router();
+
+let users = require("../models/users");
+
+router.route("/adduser").post((req,res) => {
+    const name = req.body.name;
+    const address = req.body.address;
+    const phoneNumber = req.body.phoneNumber;
+    const email = req.body.email;
+    const password = req.body.password;
+    const userType = req.body.userType;
+
+    const newUsers = new users({
+        name,
+        address,
+        phoneNumber,
+        email,
+        password,
+        userType
+    })
+
+    newUsers.save().then(()=>{
+        res.json("New User Added")
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+router.route("/getallusers").get((req,res) => {
+
+    users.find().then((users) => {
+        res.json(users)
+    }).catch((err) =>{
+        console.log(err)
+    })
+})
+
+router.route("/update/:id").put(async(req,res) => {
+
+    let userID = req.params.id;
+    const {name,address,phoneNumber,email,password,userType} = req.body;
+
+    const updateUser = {
+        name,
+        address,
+        phoneNumber,
+        email,
+        password,
+        userType
+    }
+
+    const update = await
+    users.findByIdAndUpdate(userID,updateUser).then(() => {
+        res.status(200).send({status: "User Updated"})
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({status: "Error with updating",error:err.message});
+    })
+})
+
+
+router.route("/delete/:id").delete(async(req,res) => {
+
+    let userID = req.params.id;
+
+    await users.findByIdAndDelete(userID).then(() => {
+        res.status(200).send({status: "User Deleted"});
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status: "Erro with delete user",error: err.message});
+        })
+})
+
+//get one user only
+
+router.route("/get/:id").get(async(req,res) => {
+
+    let userID = req.params.id;
+    await users.findById(userID).then((users) => {
+        res.status(200).send({status: "User fetched", users})
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({status: "Error with get user", error: err.message})
+    })
+})
+
+module.exports = router;
